e7cc10b24cf2764df00ad30b94e38c1789768c01
etienne <laurentlionel20@hotmail.com>
2023-11-25 20:15:45 +0100

    [Update] dialog menu

--- frontend/src/app/components/dialogs/menu/menu.component.ts
+++ frontend/src/app/components/dialogs/menu/menu.component.ts
@@ -1,5 +1,4 @@
-import { Component, Input } from "@angular/core";
-import { CommonModule } from "@angular/common";
+import { Component, Inject, Input } from "@angular/core";
 import { Router } from "@angular/router";
 
 interface Link {
@@ -7,25 +6,87 @@ interface Link {
   navigation: string;
 }
 
+export interface DialogData {
+  activePage: string;
+}
+
+import { FormsModule } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle
+} from "@angular/material/dialog";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { radixCross2 } from "@ng-icons/radix-icons";
+import { NgIconComponent, provideIcons } from "@ng-icons/core";
+import { CommonModule } from "@angular/common";
 @Component({
   selector: "app-menu",
   standalone: true,
-  imports: [CommonModule],
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, MatButtonModule],
   templateUrl: "./menu.component.html",
   styleUrl: "./menu.component.css"
 })
 export class MenuComponent {
+  @Input()
+  activePage!: string;
+
+  constructor(public dialog: MatDialog) {}
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      data: { activePage: this.activePage },
+      panelClass: "menu-dialog"
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log("The dialog was closed");
+    });
+  }
+}
+
+@Component({
+  selector: "dialog-overview-example-dialog",
+  templateUrl: "./menu-body.components.html",
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+    NgIconComponent
+  ],
+  viewProviders: [provideIcons({ radixCross2 })]
+})
+class DialogOverviewExampleDialog {
   links: Link[] = [
     { name: "home", navigation: "/" },
     { name: "products", navigation: "/products" }
   ];
 
-  @Input()
-  activePage!: string;
+  constructor(
+    private router: Router,
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {}
 
-  constructor(private router: Router) {}
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 
   navigate(name: string) {
+    this.onNoClick();
     this.router.navigate([name]);
   }
 }
