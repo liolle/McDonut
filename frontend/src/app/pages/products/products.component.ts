aeb96619fe3497ec1fd8489e8a7fecb34c32d19e
etienne <laurentlionel20@hotmail.com>
2023-11-26 16:52:06 +0100

    [Update]  product page

--- frontend/src/app/pages/products/products.component.ts
+++ frontend/src/app/pages/products/products.component.ts
@@ -1,13 +1,31 @@
-import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
 import { NavBarComponent } from "../../components/nav-bar/nav-bar.component";
+import { SearchBarComponent } from "../../components/search-bar/search-bar/search-bar.component";
+import { Donuts } from "../../class/donut/donut";
+import { DONUTS } from "../../services/donuts/donuts.service";
+import { DonutCardComponent } from "../../components/cards/donut-card/donut-card.component";
 
 @Component({
   selector: "app-page-products",
   standalone: true,
-  imports: [CommonModule, NavBarComponent],
+  imports: [
+    CommonModule,
+    NavBarComponent,
+    SearchBarComponent,
+    DonutCardComponent
+  ],
   templateUrl: "./products.component.html"
 })
 export class ProductsComponent {
   activePage: string = "products";
+
+  key = "";
+
+  donut: Donuts = DONUTS[0];
+
+  processKeyword(keyword: string) {
+    if (keyword.length < 2) return;
+    this.key = keyword;
+  }
 }
