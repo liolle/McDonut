5f9ca7d6fcaa44edca56da2599d216b69fe534bf
etienne <laurentlionel20@hotmail.com>
2023-11-26 19:16:11 +0100

    [Update] production - support donut observable.

--- frontend/src/app/pages/products/products.component.ts
+++ frontend/src/app/pages/products/products.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { NavBarComponent } from "../../components/nav-bar/nav-bar.component";
 import { SearchBarComponent } from "../../components/search-bar/search-bar/search-bar.component";
 import { Donuts } from "../../class/donut/donut";
-import { DONUTS } from "../../services/donuts/donuts.service";
 import { DonutCardComponent } from "../../components/cards/donut-card/donut-card.component";
+import { DonutsService } from "../../services/donuts/donuts.service";
+import { Observable } from "rxjs";
 
 @Component({
   selector: "app-page-products",
@@ -17,15 +18,20 @@ import { DonutCardComponent } from "../../components/cards/donut-card/donut-card
   ],
   templateUrl: "./products.component.html"
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
   activePage: string = "products";
+  donuts$: Observable<Donuts[]>;
 
-  key = "";
-
-  donut: Donuts = DONUTS[0];
+  constructor(private donutService: DonutsService) {}
+  ngOnInit(): void {
+    this.donuts$ = this.donutService.get(9);
+  }
 
   processKeyword(keyword: string) {
-    if (keyword.length < 2) return;
-    this.key = keyword;
+    if (keyword.length < 1) {
+      this.donuts$ = this.donutService.get(9);
+      return;
+    }
+    this.donuts$ = this.donutService.getByTerm(keyword);
   }
 }
