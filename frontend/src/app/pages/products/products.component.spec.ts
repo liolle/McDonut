c8f25e5c11e852feddaffe527e4b8c07544fa973
etienne <laurentlionel20@hotmail.com>
2023-11-27 05:14:59 +0100

    Minor changes

--- frontend/src/app/pages/products/products.component.spec.ts
+++ frontend/src/app/pages/products/products.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { ProductsComponent } from "./products.component";
+import { StoreModule } from "@ngrx/store";
 
 describe("ProductsComponent", () => {
   let component: ProductsComponent;
@@ -8,7 +9,7 @@ describe("ProductsComponent", () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ProductsComponent]
+      imports: [StoreModule.forRoot({}), ProductsComponent]
     }).compileComponents();
 
     fixture = TestBed.createComponent(ProductsComponent);
