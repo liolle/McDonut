0fb8afdddcfc753b92cbe7deca4e053aa5ef343e
etienne <laurentlionel20@hotmail.com>
2023-11-27 05:18:46 +0100

    [Update] production page - support cart selector

--- frontend/src/app/pages/products/products.component.ts
+++ frontend/src/app/pages/products/products.component.ts
@@ -1,11 +1,18 @@
 import { CommonModule } from "@angular/common";
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, inject } from "@angular/core";
+import { NgIconComponent, provideIcons } from "@ng-icons/core";
+import { featherShoppingCart } from "@ng-icons/feather-icons";
+import { Store } from "@ngrx/store";
+import { Observable } from "rxjs/internal/Observable";
+import { Donuts } from "../../class/donut/donut";
+import { CartComponent } from "../../components/cards/cart/cart.component";
+import { DonutCardComponent } from "../../components/cards/donut-card/donut-card.component";
 import { NavBarComponent } from "../../components/nav-bar/nav-bar.component";
 import { SearchBarComponent } from "../../components/search-bar/search-bar/search-bar.component";
-import { Donuts } from "../../class/donut/donut";
-import { DonutCardComponent } from "../../components/cards/donut-card/donut-card.component";
+import { Cart } from "../../interfaces/cart";
 import { DonutsService } from "../../services/donuts/donuts.service";
-import { Observable } from "rxjs/internal/Observable";
+import { CartDialogComponent } from "../../components/dialogs/cart/cart-dialog.component";
+import { selectCart } from "../../shared/cart/cart.reducer";
 
 @Component({
   selector: "app-page-products",
@@ -14,17 +21,25 @@ import { Observable } from "rxjs/internal/Observable";
     CommonModule,
     NavBarComponent,
     SearchBarComponent,
-    DonutCardComponent
+    DonutCardComponent,
+    CartComponent,
+    NgIconComponent,
+    CartDialogComponent
   ],
-  templateUrl: "./products.component.html"
+  templateUrl: "./products.component.html",
+  viewProviders: [provideIcons({ featherShoppingCart })]
 })
 export class ProductsComponent implements OnInit {
   activePage: string = "products";
   donuts$: Observable<Donuts[]>;
 
+  private readonly store: Store<{ cart: Cart }> = inject(Store);
+  cart$: Observable<Cart>;
+
   constructor(private donutService: DonutsService) {}
   ngOnInit(): void {
     this.donuts$ = this.donutService.get(9);
+    this.cart$ = this.store.select(selectCart);
   }
 
   processKeyword(keyword: string) {
