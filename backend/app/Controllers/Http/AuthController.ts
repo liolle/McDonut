c2be54843b6254436fb6b4c97353c9bc01c9b983
etienne <laurentlionel20@hotmail.com>
2023-11-30 02:13:38 +0100

    [Update] auth controller - add OAUTH2 google - update OAT login

--- backend/app/Controllers/Http/AuthController.ts
+++ backend/app/Controllers/Http/AuthController.ts
@@ -21,9 +21,18 @@ export default class AuthController {
   public async login({ auth, request, response }: HttpContextContract) {
     const { email, password } = request.only(['email', 'password'])
 
+    if (!email || !password)
+      return response.unauthorized({ message: 'password or email incorrect' })
+
     try {
-      const token = await auth.attempt(email, password)
-      return token
+      const token = await auth.attempt(email, password, {
+        expiresIn: '90 mins',
+      })
+      response.cookie('sessionId', token.token)
+
+      return response.json({
+        token: token.token,
+      })
     } catch (error) {
       return response.unauthorized({ message: 'password or email incorrect' })
     }
@@ -31,8 +40,84 @@ export default class AuthController {
 
   public async logout({ auth, response }: HttpContextContract) {
     await auth.use('api').revoke()
+    response.clearCookie('sessionId')
     return response.json({
       revoked: true,
     })
+  }
+
+  public async googleRedirect({ ally }: HttpContextContract) {
+    return ally.use('google').redirect()
+  }
+
+  public async googleCallback({ ally, auth, response }: HttpContextContract) {
+    const google = ally.use('google')
+
+    /**
+     * User has explicitly denied the login request
+     */
+    if (google.accessDenied()) {
+      return 'Access was denied'
+    }
+
+    /**
+     * Unable to verify the CSRF state
+     */
+    if (google.stateMisMatch()) {
+      return 'Request expired. Retry again'
+    }
+
+    /**
+     * There was an unknown error during the redirect
+     */
+    if (google.hasError()) {
+      console.log(google)
+
+      return google.getError()
+    }
+
+    /**
+     * Finally, access the user
+     */
+
+    const googleUser = await google.user()
+
+    if (!googleUser.email)
+      return response.json({
+        status: false,
+        message: 'Something went wrong.',
+      })
+    /**
+     * Find the user by email or create
+     * a new one
+     */
+    const user = await User.firstOrCreate(
+      {
+        email: googleUser.email,
+      },
+      {
+        email: googleUser.email,
+        provider: 'google',
+        access_token: googleUser.token.token,
+      }
+    )
+
+    const token = await auth.use('api').generate(user, {
+      expiresIn: '90 mins',
+    })
+
+    response.cookie('sessionId', token)
+
+    return response
+  }
+
+  public async me({ auth }: HttpContextContract) {
+    const { email, role, createdAt } = auth.user
+
+    return {
+      email,
+      role,
+      createdAt,
+    }
   }
 }
