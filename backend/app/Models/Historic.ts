6aba596096e266b56577359bc936012ef28a2d65
etienne <laurentlionel20@hotmail.com>
2023-12-03 12:55:12 +0100

    [Update] tables id

--- backend/app/Models/Historic.ts
+++ backend/app/Models/Historic.ts
@@ -1,13 +1,19 @@
-import { BaseModel, HasOne, column, hasOne } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, HasOne, beforeCreate, column, hasOne } from '@ioc:Adonis/Lucid/Orm'
 import { DateTime } from 'luxon'
 import User from './User'
+import { IdGenerator } from '../class/idGenerator'
 
 export default class Historic extends BaseModel {
   @column({ isPrimary: true })
-  public id: number
+  public id: string
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
+
+  @beforeCreate()
+  public static async setID(historic: Historic) {
+    historic.id = IdGenerator.generate(IdGenerator.HISTORIC_ID)
+  }
 
   @hasOne(() => User)
   public user: HasOne<typeof User>
