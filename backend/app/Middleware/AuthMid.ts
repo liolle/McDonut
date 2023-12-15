1a8a2ccc4a74ea1b8eb987d2322084d2a1842412
liolle <44002141+liolle@users.noreply.github.com>
2023-12-15 15:31:03 +0100

    []

--- backend/app/Middleware/AuthMid.ts
+++ backend/app/Middleware/AuthMid.ts
@@ -4,6 +4,8 @@ export default class AuthMid {
   public async handle({ request }: HttpContextContract, next: () => Promise<void>) {
     const token = request.cookie('sessionId')
 
+    console.log('AuthMid List', request.cookiesList())
+
     if (request.headers().authorization) {
       await next()
       return
