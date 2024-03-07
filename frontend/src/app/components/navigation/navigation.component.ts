4c17f6c4dda5e847dfc593a19949131588307508
liolle <44002141+liolle@users.noreply.github.com>
2024-03-07 21:30:33 +0100

    [Update] favicon (#130)
    * [Update] navigation
    
    * [Update] favicon
    
    * [Update] node version
    
    * [Update] pnpm version
    
    set version: 8.12.0

--- frontend/src/app/components/navigation/navigation.component.ts
+++ frontend/src/app/components/navigation/navigation.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from "@angular/common";
-import { Component, inject } from "@angular/core";
+import { Component, OnInit, inject } from "@angular/core";
 import { Router } from "@angular/router";
 import { Store } from "@ngrx/store";
+import { Observable, Subject, flatMap, of, switchMap } from "rxjs";
+import { UserProfile } from "../../interfaces/api";
+import { UserS } from "../../shared/auth/reducer";
 import { GeneralS } from "../../shared/reducer";
-import { selectPage } from "../../shared/selector";
-import { of } from "rxjs";
+import { selectPage, selectUser } from "../../shared/selector";
 
 interface Link {
   name: string;
@@ -17,16 +19,36 @@ interface Link {
   imports: [CommonModule],
   templateUrl: "./navigation.component.html"
 })
-export class PageNavigationComponent {
-  links: Link[] = [
-    { name: "home", navigation: "/" },
-    { name: "products", navigation: "/products" }
-  ];
+export class PageNavigationComponent implements OnInit {
+  links$ = of([
+    { name: "home", navigation: "/", available: true },
+    { name: "products", navigation: "/products", available: true },
+    { name: "profile", navigation: "/profile", available: true }
+  ]);
 
-  private store: Store<{ general: GeneralS }> = inject(Store);
+  private store: Store<{ general: GeneralS; user: UserS }> = inject(Store);
   activePage$ = this.store.select(selectPage) || of("/");
 
+  user$: Observable<UserProfile>;
+
   constructor(private router: Router) {}
+  ngOnInit(): void {
+    this.user$ = this.store.select(selectUser).pipe(
+      switchMap((value) => {
+        return of(value.user);
+      })
+    );
+
+    this.user$.subscribe((value) => {
+      if (value.email != "") {
+        this.links$ = of([
+          { name: "home", navigation: "/", available: true },
+          { name: "products", navigation: "/products", available: true },
+          { name: "profile", navigation: "/profile", available: true }
+        ]);
+      }
+    });
+  }
 
   navigate(name: string) {
     this.router.navigate([name]);
